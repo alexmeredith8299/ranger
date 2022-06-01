7bdd46a8e87dce5f7150209fd52ae74d91df4c45
Alex Meredith <ameredit@mit.edu>
2022-06-01 11:25:22 -0400

    pushing pi binary and model trained on pi, will test docker cc to pi later

--- alex_tests/test_training_rf.sh
+++ alex_tests/test_training_rf.sh
@@ -1,4 +1,4 @@
 #!bin/sh
-./../cpp_version/bin-ccc-arm/ranger --file train_png_small_set --mask train_mask_png_small_set --depvarname CLOUD --treetype 1 --probability --ntree 10 --memmode 3 --savemem --maxdepth 18 --write --verbose --batch
+./../cpp_version/bin-ccc-arm/ranger --file train_png_small_set --mask train_mask_png_small_set --depvarname CLOUD --treetype 1 --probability --ntree 10 --memmode 3 --maxdepth 18 --write --verbose --batch
 #./../cpp_version/bin-ccc-arm/ranger --file test.csv --mask test.csv --depvarname CLOUD --treetype 1 --probability --ntree 10 --memmode 2 --savemem --maxdepth 18 --write --verbose
 #./../cpp_version/bin-ccc-arm/ranger --file samples/img_msec_1597559030639_2_thumbnail.jpeg --mask samples/img_msec_1597559030639_2_thumbnail.jpeg --depvarname CLOUD --treetype 1 --probability --ntree 10 --memmode 3 --savemem --maxdepth 18 --write --verbose
