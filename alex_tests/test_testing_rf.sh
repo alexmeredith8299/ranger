9a38c6ba6b525b113bdcc1192c1fd91df4f04806
alex meredith <alexm0802@gmail.com>
2022-06-03 10:44:06 -0400

    new ranger no print

--- alex_tests/test_testing_rf.sh
+++ alex_tests/test_testing_rf.sh
@@ -1,3 +1,3 @@
 #!bin/sh
-./../cpp_version/bin-ccc-arm/ranger --predict ranger_out_pi_5img.forest --file samples/img_msec_1597559030639_2_thumbnail.jpeg --writetoimg
+./../cpp_version/bin/ranger --predict ranger_out_int.forest --file samples/img_msec_1597559030639_2_thumbnail.jpeg --writetoimg
 
