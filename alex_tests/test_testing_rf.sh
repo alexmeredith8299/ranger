b70aa406985cf077336df5568ab81a7626adc0e5
Alex Meredith <ameredit@mit.edu>
2022-07-12 16:12:59 -0400

    update test_testing_rf.sh

--- alex_tests/test_testing_rf.sh
+++ alex_tests/test_testing_rf.sh
@@ -1,3 +1,3 @@
 #!bin/sh
-./../cpp_version/bin-ccc-arm/ranger --predict ranger_out_pi_5img.forest --file samples/img_msec_1597559030639_2_thumbnail.jpeg --writetoimg
+./../cpp_version/bin-arm-mfpu/ranger --predict ranger_out.forest --file samples/img_msec_1597559030639_2_thumbnail.jpeg --writetoimg
 
