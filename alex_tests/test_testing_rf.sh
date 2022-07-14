4be392cfd61558a97f44cd69ca91e5453cc65683
Alex Meredith <alexm0802@gmail.com>
2022-07-14 11:09:18 -0400

    new sample for segmentation on pi

--- alex_tests/test_testing_rf.sh
+++ alex_tests/test_testing_rf.sh
@@ -1,3 +1,3 @@
 #!bin/sh
-./../cpp_version/bin-arm-mfpu/ranger --predict ranger_out.forest --file samples/img_msec_1651278782370_2_thumbnail.jpeg --writetoimg
+./../cpp_version/bin-arm-mfpu/ranger --predict ranger_out.forest --file samples/img_msec_1651265707219_2_thumbnail.jpeg --writetoimg
 
